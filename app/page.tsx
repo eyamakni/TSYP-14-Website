df7dc5b0c7230ee524fd16858b20a275d6459d59
eyamakni <makni.eya03@gmail.com>
2026-07-03 23:12:54 +0100

    about us

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,13 @@
 import HeroSection from "@/components/home/HeroSection";
 import CountdownSection from "@/components/home/CountdownSection";
+import AboutSection from "@/components/home/AboutSection";
 export default function Home() {
   return (
     <>
       <main>
         <HeroSection />
         <CountdownSection />
+        <AboutSection />
       </main>
 
     </>
